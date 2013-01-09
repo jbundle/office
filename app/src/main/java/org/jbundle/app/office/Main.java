40ae89caf61bf079bda0876a7e6330ee0a91d846
Don Corley <don@donandann.com>
2013-01-08 22:44:07 -0800

    Add thick main class

---
+++ app/src/main/java/org/jbundle/app/office/Main.java
@@ -0,0 +1,18 @@
+package org.jbundle.app.office;
+
+import org.jbundle.app.office.contact.screen.*;
+// import org.jbundle.res.app.office.contact.db.*;
+import org.jbundle.app.office.calendar.screen.*;
+import org.jbundle.app.office.calendar.screen.view.swing.*;
+import org.jbundle.thin.app.office.contact.db.*;
+import org.jbundle.thin.app.office.calendar.*;
+
+public class Main extends org.jbundle.Main {
+
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String args[])
+    {
+        org.jbundle.Main.main(args);
+    }
+}
